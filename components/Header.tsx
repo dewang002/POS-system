ef5ce72fa3f26ab1630829a5cd313aec0e9850aa
dewang002 <2002thapa@gmail.com>
2025-05-27 16:49:38 +0530

    fixed logo and add user logged in logic

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,21 +3,26 @@ import React from 'react';
 import { UserButton } from '@clerk/nextjs';
 import { Button } from './ui/button';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Header = () => {
     return (
         <header className="w-full shadow-md bg-white px-6 py-4 flex items-center justify-between">
             <div className="flex items-center space-x-2">
-                <span className="text-xl font-semibold text-gray-800">YourBrand</span>
+                <Image className='rounded-full' width={40} height={40} src={'/logo.png'} alt='logo' />
             </div>
 
             <div className='flex gap-4'>
-                <Link href={'/sign-in'}>
-                    <Button>Sign In</Button>
-                </Link>
-                <Link href={'/sign-up'}>
-                    <Button>Sign Up</Button>
-                </Link>
+                {
+                    UserButton ? <UserButton /> : <>
+                            <Link href={'/sign-in'}>
+                                <Button>Sign In</Button>
+                            </Link>
+                            <Link href={'/sign-up'}>
+                                <Button>Sign Up</Button>
+                            </Link>
+                    </>
+                }
             </div>
         </header>
     );
