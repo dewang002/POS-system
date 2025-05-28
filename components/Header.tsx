4d99b493db5d49230c42d70cc5197a31f22f31b6
dewang002 <2002thapa@gmail.com>
2025-05-28 21:44:56 +0530

    doen

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,20 +7,22 @@ import Image from 'next/image';
 
 const Header = () => {
     return (
-        <header className="w-full shadow-md bg-white px-6 py-4 flex items-center justify-between">
+        <header className="w-full shadow-md bg-black bg-opacity-50 backdrop-blur-md border-opacity-20  px-6 py-4 flex items-center justify-between">
             <div className="flex items-center space-x-2">
                 <Image className='rounded-full' width={40} height={40} src={'/logo.png'} alt='logo' />
             </div>
-
+            <div className='w-md'>
+                <input className='border border-gray-300 bg-white bg-opacity-30 w-full p-1 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-300' placeholder='search . . .' />
+            </div>
             <div className='flex gap-4'>
                 {
                     UserButton ? <UserButton /> : <>
-                            <Link href={'/sign-in'}>
-                                <Button>Sign In</Button>
-                            </Link>
-                            <Link href={'/sign-up'}>
-                                <Button>Sign Up</Button>
-                            </Link>
+                        <Link href={'/sign-in'}>
+                            <Button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg transition duration-300 ease-in-out">Sign In</Button>
+                        </Link>
+                        <Link href={'/sign-up'}>
+                            <Button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg transition duration-300 ease-in-out">Sign Up</Button>
+                        </Link>
                     </>
                 }
             </div>
--- components/Home.tsx
+++ components/Home.tsx
@@ -1,16 +1,41 @@
 "use client"
 
+import { useEffect, useState } from "react"
 import BottomNav from "./BottomNav"
 
 const Home = () => {
+    const [Datetime, setDateTime] = useState(new Date())
+
+    useEffect(() => {
+        const time = setInterval(() => setDateTime(new Date()), 1000)
+    }, [])
+
+    const formatDateTime = (date: Date) => {
+        return date.toLocaleString('en-US', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit'
+        })
+    }
+
     return (
         <div className="h-screen w-full overflow-hidden ">
             <div className="flex h-full w-full">
-                <div className="flex-3 bg-amber-800">
-                    
+                <div className="flex-3 flex text-white px-4 bg-zinc-800">
+                    <div className="w-full ">
+                        <h1 className="text-2xl font-bold">Hello, </h1>
+                        <p className="text-lg text-light">Give your best services for customers 😄</p>
+                    </div>
+                    <div>
+                        <h1>{formatDateTime(Datetime)}</h1>
+                    </div>
                 </div>
                 <div className="flex-1 bg-pink-800">
-                    
+
                 </div>
             </div>
 
