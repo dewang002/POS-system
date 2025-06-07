c268a6a8fc5ea661746b7439a711e2798925a088
dewang002 <2002thapa@gmail.com>
2025-06-07 13:28:00 +0530

    done

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,4 +1,3 @@
-import Header from '@/components/Header'
 import Home from '@/components/Home'
 import React from 'react'
 
--- components/Home.tsx
+++ components/Home.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import BottomNav from "./BottomNav"
 import {
   Card,
   CardHeader,
